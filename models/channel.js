30d24a08f18510aed572388b0d72f242e1f57512
Joseph Dorman <JDorman555@yahoo.com>
2024-02-09 15:41:29 -0600

    feat: add channel schema model

---
+++ models/channel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ChannelSchema = new Schema({
+  name: { type: String, require: true },
+  users: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
+  lastMessage: { type: String, require: true },
+});
+
+module.exports = mongoose.model("Channel", ChannelSchema);

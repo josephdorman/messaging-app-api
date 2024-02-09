e52f450a7792418aab8b7d32c816f80d5c56dafb
Joseph Dorman <JDorman555@yahoo.com>
2024-02-09 15:46:12 -0600

    feat: add user schema model

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: { type: String, require: true },
+  password: { type: String, require: true },
+  profileIMG: { type: String, default: "defaultProfile.jpg" },
+  friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  channels: [{ type: Schema.Types.ObjectId, ref: "Channel" }],
+  messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
+});
+
+module.exports = mongoose.model("User", UserSchema);

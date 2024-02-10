42ee36cb7278e1b5cd1ef235afb628abf080b1c0
Joseph Dorman <JDorman555@yahoo.com>
2024-02-09 20:19:34 -0600

    feat: import controller modules for api routing

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+// Require controller modules.
+const userController = require("../controllers/userController");
+const channelController = require("../controllers/channelController");
+
 /* GET api index. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");

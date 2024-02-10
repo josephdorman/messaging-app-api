1ffa3407c74c8c566656620ff0ab93b64c3b781f
Joseph Dorman <JDorman555@yahoo.com>
2024-02-09 21:44:26 -0600

    feat: add user authentication

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const User = require("./models/user");
 
 const indexRouter = require("./routes/index");
 const apiRouter = require("./routes/api");
@@ -16,6 +21,28 @@ const app = express();
 mongoose.connect(CONNECTION_URL);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// Passport Authentication
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username." });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+
+      if (!match) {
+        return done(null, false, { message: "Incorrect password." });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
 
 app.use(logger("dev"));
 app.use(express.json());

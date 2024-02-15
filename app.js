f9f5ec49d00bef8770d8f74bd9f810aca2ac745d
Joseph Dorman <JDorman555@yahoo.com>
2024-02-14 23:52:37 -0600

    feat: enable origin and credential requirements

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ passport.use(
   )
 );
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

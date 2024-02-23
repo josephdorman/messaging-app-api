3d8e4a886ffc7328e55b03a9c143451ab840f6a2
Joseph Dorman <JDorman555@yahoo.com>
2024-02-23 00:32:40 -0600

    chore: remove useless import

--- middleware/cookieJwtAuth.js
+++ middleware/cookieJwtAuth.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const asyncHandler = require("express-async-handler");
-const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
 exports.cookieJwtAuth = asyncHandler(async (req, res, next) => {
@@ -8,9 +7,6 @@ exports.cookieJwtAuth = asyncHandler(async (req, res, next) => {
   try {
     const user = jwt.verify(token, process.env.SECRET_KEY);
     req.user = user;
-    const currentUser = await User.findById(user.id, "_id username profileIMG");
-    console.log(currentUser);
-    res.json(currentUser);
     next();
   } catch (err) {
     res.clearCookie("token");

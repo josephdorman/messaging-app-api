39e828822957eea9edc6a89277e68c21b9ec2d9c
Joseph Dorman <JDorman555@yahoo.com>
2024-03-05 23:41:02 -0600

    feat: manage user online status

--- socket.js
+++ socket.js
@@ -8,10 +8,27 @@ const socketServer = (server) => {
     },
   });
 
+  let onlineUsers = [];
+
   io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
 
+    socket.on("user_online", (data) => {
+      if (!onlineUsers.some((user) => user.userId === data.id)) {
+        onlineUsers.push({ userId: data.id, socketId: socket.id });
+      }
+
+      io.emit("receive_friends", onlineUsers);
+    });
+
+    socket.on("get_friends", () => {
+      io.emit("receive_friends", onlineUsers);
+    });
+
     socket.on("disconnect", () => {
+      onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+
+      io.emit("receive_friends", onlineUsers);
       console.log(`User Disconnected: ${socket.id}`);
     });
   });
